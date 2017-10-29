a4755a0d3a90775d8ce6971b1fc2f80bcbcceb33
Alexey Dulenko <dulenkol@gmail.com>
2017-10-29 23:23:25 +0200

    improve new project component

--- src/js/components/NewProject.js
+++ src/js/components/NewProject.js
@@ -3,8 +3,12 @@ let input;
 class NewProject extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { isEdit: false };
+
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   };
 
   handleSubmit(e) {
@@ -16,6 +20,11 @@ class NewProject extends Component {
   handleCancel(e) {
     e.preventDefault();
     input.value = '';
+    this.handleChange(e)
+  }
+
+  handleChange(e) {
+    this.setState({ isEdit: !!input.value });
   }
 
   render() {
@@ -25,10 +34,11 @@ class NewProject extends Component {
           type='text'
           placeholder='Enter Project Name...'
           ref={node => { input = node }}
+          onChange={this.handleChange}
         />
 
-        <button onClick={this.handleSubmit}>Save</button>
-        <button onClick={this.handleCancel}>Cancel</button>
+        { this.state.isEdit && <button onClick={this.handleSubmit}>Save</button> }
+        { this.state.isEdit && <button onClick={this.handleCancel}>Cancel</button> }
       </div>
     );
   };
