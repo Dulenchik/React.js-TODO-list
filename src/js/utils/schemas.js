a4ea789a4410975d2a8cb2df856527204e5f3747
Alexey Dulenko <dulenkol@gmail.com>
2018-07-03 00:00:35 +0300

    Add normalizer

---
+++ src/js/utils/schemas.js
@@ -0,0 +1,35 @@
+import { schema } from "normalizr"
+import moment from "moment"
+
+export const projectSchema = new schema.Entity(
+  "projects",
+  {},
+  {
+    processStrategy: (value, parent, key) => {
+      return { id: value.id, name: value.name }
+    }
+  }
+)
+
+export const tasksSchema = new schema.Entity(
+  "tasks",
+  {},
+  {
+    processStrategy: (
+      {
+        id,
+        name,
+        deadline,
+        priority: position,
+        project_id: projectId,
+        done: isDone
+      },
+      parent,
+      key
+    ) => {
+      const dueDate = deadline ? moment(deadline).format("DD/MM/YYYY") : null
+      const dueTime = deadline ? moment(deadline).format("HH:mm") : null
+      return { id, name, position, projectId, isDone, dueDate, dueTime }
+    }
+  }
+)
